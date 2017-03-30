76020f1d02dce08a52e121e2fdeae3a5ba44bb50
Seolhee Kim <seolhee@upspringmedia.com>
2017-03-29 20:41:29 -1000

    Hw Done

--- js/app.js
+++ js/app.js
@@ -2,3 +2,18 @@
 /*Add event listeners to each restaurant name that will show and hide (toggle) the secret menu item for that particular restaurant.*/
 
 
+
+var showMenu = document.getElementsByClassName("name");
+
+for (var i = 0; i<showMenu.length; i++){
+  showMenu[i].addEventListener("click", displayImg);
+}
+
+function displayImg() {
+  var getImg = this.querySelectorAll(".menu")[0];
+    if(getImg.style.display === "none"){
+      getImg.style.display = "block";
+    }else {
+      getImg.style.display = "none";
+    }
+}
